ccacecc622bb7065e0963b34f78de23278f70ec6
Yair Gadelov <yair.gadelov@gmail.com>
2023-08-12 20:54:50 +0300

    added lua support

--- src/main.c
+++ src/main.c
@@ -33,13 +33,49 @@ struct gpio_dt_spec en_station[] = {
 };
 
 
+#include <zephyr/fs/fs.h>
+
+#include <stdlib.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+
+int _open() {
+	return 0;
+}
+
+const char *lua = " \
+for i = 10,1,-1 \
+do \
+   print(i, 'str ' .. tostring(i)) \
+   --[[ assert((i %  2) == 0)  --]] \
+end \
+";
 int main(void)
 {
 
 		
+	lua_State *L = luaL_newstate();
+  	luaL_openlibs(L);
+  	int n1 = luaL_dostring(L, "print('Hello from Lua')");
+	(void)n1;
+  	int n2 = luaL_dostring(L, "a=1;b=1;print('assert', assert(a==b))");
+	(void)n2;
+  	int n3 = luaL_dostring(L, lua);
+	(void)n3;
+
+
+
+
+	//printk("Hello with Lua!\n");
+
+	lua_close(L);
+	
 	// configure the LED pin as output
 	for (int i =0;i < 2; i++)
-	  	gpio_pin_configure_dt(&specs[i], GPIO_OUTPUT);
+	  	gpio_pin_configure_dt(&en_station[i], GPIO_OUTPUT);
 
 
 	printk("Hello World! %s\n", CONFIG_BOARD);

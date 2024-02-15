d7926330130a65d35c16c4efb547899c9e7e395f
Yair Gadelov <yair.gadelov@gmail.com>
2024-02-15 13:21:16 +0200

    Added missing file

---
+++ game/lua-5.4.6/memory.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  memory.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/15/2024 10:14:45 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yair Gadelov (yg), yair.gadelov@gmail.com
+ *        Company:  Israel
+ *
+ * =====================================================================================
+ */
+#include "stdlib.h"
+
+/**
+ * Created  02/15/2024
+ *  @brief This function serves as a wrapper for realloc within the Lua API. This replacement is essential to facilitate compatibility with the Zephyr HAL. 
+ * @note  
+ * @param   
+ * @return  
+ */
+char * lua_realloc(char *ptr, size_t n ) {
+	return realloc(ptr, n);
+}
+
+/**
+ * Created  02/15/2024
+ *  @brief This function serves as a wrapper for free within the Lua API. This replacement is essential to facilitate compatibility with the Zephyr HAL. 
+ * @note  
+ * @param   
+ * @return  
+ */
+void lua_free(void *ptr) {
+	free(ptr);
+}
+
+
